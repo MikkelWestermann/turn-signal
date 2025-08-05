67b89b6b3304e8453788f26b3c83cd5e1ad28f07
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2025-08-05 17:13:02 +0200

    Add link to turn signal roadmap

--- components/marketing/navbar.tsx
+++ components/marketing/navbar.tsx
@@ -97,7 +97,7 @@ export function MarketingNavbar() {
                       >
                         TLDR: It's free
                       </ListItem>
-                      <ListItem href="/roadmap" title="Roadmap">
+                      <ListItem href="/roadmap/turn-signal" title="Roadmap">
                         See what we're building next
                       </ListItem>
                     </ul>
@@ -171,7 +171,7 @@ export function MarketingNavbar() {
                     Pricing
                   </Link>
                   <Link
-                    href="/roadmap"
+                    href="/roadmap/turn-signal"
                     className="block px-3 py-2 text-sm text-muted-foreground hover:text-foreground"
                   >
                     Roadmap
